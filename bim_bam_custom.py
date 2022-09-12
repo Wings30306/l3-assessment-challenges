ad551a938d7870fdce6ac761c43897eb5214d688
Joke Heyndels <jo_hannah@outlook.com>
2022-09-12 13:20:02 +0000

    Replace FizzBuzz with BimBam

---
+++ bim_bam_custom.py
@@ -0,0 +1,67 @@
+"""
+Bim Bam custom:
+    1. Loop through a data structure containing numbers
+    and print BimBam result for each
+    2. Get user inputs for name and age, tell user the BimBam result
+    for their age in a string
+
+BimBam rules:
+    - if number is divisible by 3, return "Bim"
+    - if number is divisible by 5, return "Bam"
+    - if number is divisible by both 3 and 5, return "BimBam"
+    - if number is not divisible by either 3 or 5, return the number.
+"""
+
+
+def bim_bam(num):
+    """
+    Calculate BimBam result
+    """
+    if num % 2 == 0 and num % 7 == 0:
+        result = "BimBam"
+    elif num % 2 == 0:
+        result = "Bim"
+    elif num % 7 == 0:
+        result = "Bam"
+    else:
+        result = num
+    return result
+
+
+def loop_mode():
+    """
+    Loop through a list of numbers and return the result
+    """
+    nums = [1, 2, 3, 5, 7, 12, 19, 28, 31, 42]
+    for num in nums:
+        print(f"In BimBam, {num} would return {bim_bam(num)}")
+
+
+def custom_mode():
+    """
+    Get user's name and age, return string to tell them the BimBam result
+    for their age
+    """
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    result = bim_bam(age)
+    print(f"{name}, you are {age} years old.")
+    print(f"In BimBam, this number would return {result}")
+
+
+def play():
+    """
+    Explain modes and let user choose
+    """
+    print("Hi there, welcome to BimBam Custom")
+    print("We have two modes for you to choose from:")
+    print("1. Loop through a list of numbers to see what BimBam returns.")
+    print("2. Enter your age and see what the BimBam result for that is.")
+    mode = int(input("Which would you like to try? (1/2) "))
+    if mode == 1:
+        loop_mode()
+    else:
+        custom_mode()
+
+
+play()
